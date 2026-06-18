16bce11d3f943ac7c32388dd49acb07fe7a311bc
blueapple373-png <blueapple373@gmail.com>
2026-06-18 13:06:21 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import LP from './LP.jsx'
+
+const path = window.location.pathname
+const Page = path === '/lp' ? LP : App
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Page />)
