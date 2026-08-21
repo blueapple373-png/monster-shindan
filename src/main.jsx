567830ed9e4cc1435c03ef58f4e3f70c7e551850
blueapple373-png <blueapple373@gmail.com>
2026-08-21 14:10:15 +0900

    Load blog article styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LP from './LP.jsx'
 import Site from './Site.jsx'
+import './blog.css'
 
 const path = window.location.pathname.replace(/\/$/, '') || '/'
 
